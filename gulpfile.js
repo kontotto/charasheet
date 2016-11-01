8c9bbdb69bbd23350f6077395b38a4deb40c9632
Shuta Kondo <takekon591@gmail.com>
2016-11-01 17:46:40 +0900

    [add] gulpを追加

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+const gulp = require('gulp');
+const ect = require('gulp-ect');
+const babel = require('gulp-babel');
+const data = require('./server/data.json');
+
+gulp.task('test', function(){
+  console.log(data);
+});
+
+gulp.task('html', function(){
+  gulp.src('./src/*.ect')
+      .pipe(ect({data: function(file, cb){
+        cb({data: data});
+      }}))
+      .pipe(gulp.dest('./public'));
+});
+
+gulp.task('js', function(){
+  gulp.src('./src/js/*.js')
+      .pipe(babel())
+      .pipe(gulp.dest('./public/js'));
+});
+
+gulp.task('watch', function(){
+  gulp.watch('./src/js/*.js', ['js']);
+  gulp.watch('./src/*.ect', ['html']);
+});
+
+
+
+gulp.task('default', ['test', 'html', 'js']);
